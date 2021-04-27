9534bc1bfe592340fbb8f86171cca3f321b0a66c
omar chatin <66512898+omer-os@users.noreply.github.com>
2021-04-27 07:54:03 +0300

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,21 @@
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyA3dWN1eCdgw23YTLu6e4smzhWWKxiX8Rc",
+//   authDomain: "newbee-aa72d.firebaseapp.com",
+//   projectId: "newbee-aa72d",
+//   storageBucket: "newbee-aa72d.appspot.com",
+//   messagingSenderId: "234613834095",
+//   appId: "1:234613834095:web:d09b5ffc021b258477e89a",
+//   measurementId: "G-VG9PZG2SNZ"
+// };
+
+
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const db = firebase.firestore()
+
+// export default db;
