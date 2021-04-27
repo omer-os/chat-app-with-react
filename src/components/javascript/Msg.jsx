54474cba686d8dfb09e3cd67adad53a87713dae3
omar chatin <66512898+omer-os@users.noreply.github.com>
2021-04-27 11:03:45 +0300

    fetchin data from firebase

---
+++ src/components/javascript/Msg.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Msg() {
+    return (
+        <div className='msg'>
+            
+            <div className="name">omar chatin : </div>
+            <div className="text">lodddddddd</div>
+        </div>
+    );
+}
--- src/components/javascript/structure.jsx
+++ src/components/javascript/structure.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import structure from '../styles/structure.css'
+import Msg from "./Msg"
+import db from './firebase.config'
 
 export default function Structure() {
+
+    const [Text, setText] = useState('')
+
+    const getData = async()=>{
+        const res = db.ref('chat')
+        res.on('value',(snapshot)=>{
+          const u = snapshot.val()
+          const usersList =[]
+          for (let  id in u){
+            usersList.push(u[id])
+          }
+          console.log(usersList);
+      })
+
+
+    }
+    useEffect(()=>{
+        getData()
+    },[])
 
     return (
         <div className='structure'>
@@ -11,9 +32,12 @@ export default function Structure() {
                     <p className='navText'>awesome room</p>
                 </nav>
 
-                <div className="messegeingSection"></div>
+                <div className="messegeingSection">
+                    <Msg />
+                    <Msg />
+                </div>
                 <div className="inputSection">
-                    <input type="text" className="text"/>
+                    <input type="text" className="inputText" onChange={inputTextHandeler}/>
                     <button className="sendBtn">send</button>
                 </div>
             </div>
